1432f0dfb75febe82d9f9f3d525848891e300a3f
Kovács Bence <hobbemail0@gmail.com>
2023-09-18 19:45:43 +0200

    masodik feladat jo

--- feladat2.js
+++ feladat2.js
@@ -1,4 +1,24 @@
-szovegSzin(){
+function zoldSzovegSzin(){
     const szoveg = document.getElementById('szoveg');
-    szoveg.style.color = 'red';
+    szoveg.style.color = 'green';
+}
+function feketeSzovegSzin(){
+    const szoveg = document.getElementById('szoveg');
+    szoveg.style.color = 'black';
+}
+function zoldSzovegSzin2(){
+    const szoveg = document.getElementById('szoveg2');
+    szoveg2.style.color = 'green';
+}
+function feketeSzovegSzin2(){
+    const szoveg = document.getElementById('szoveg2');
+    szoveg2.style.color = 'black';
+}
+function zoldSzovegSzin3(){
+    const szoveg = document.getElementById('szoveg3');
+    szoveg3.style.color = 'green';
+}
+function feketeSzovegSzin3(){
+    const szoveg = document.getElementById('szoveg3');
+    szoveg3.style.color = 'black';
 }
