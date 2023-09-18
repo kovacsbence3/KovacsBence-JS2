13482ed0fe985c2621c0187425c9efc7cfe45197
Kovács Bence <hobbemail0@gmail.com>
2023-09-18 19:17:41 +0200

    feladat3 kész

---
+++ feladat3.js
@@ -0,0 +1,10 @@
+function betoltottKep(){
+    const szoveg = document.getElementById('szoveg');
+    szoveg.textContent = "Betöltődött";
+}
+function rakattintott(){
+    
+    const szoveg2 = document.getElementById('szoveg');
+    szoveg2.textContent = "Rákattintott";
+
+}
